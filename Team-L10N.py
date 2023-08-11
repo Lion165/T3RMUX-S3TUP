deccc04513338072e55639261486637357360e53
TEAM-L10N <140788132+Lion165@users.noreply.github.com>
2023-08-11 05:13:35 +0300

    Add files via upload

---
+++ Team-L10N.py
@@ -0,0 +1,124 @@
+import os
+import sys
+import bs4
+import requests
+import mechanize
+os.system('clear')
+
+
+logo =                                                          ("""
+\033[1;36m ________________________________________________________
+
+  _______ ______          __  __   _     __  ___  _   _
+ |__   __|  ____|   /\   |  \/  | | |   /_ |/ _ \| \ | |
+    | |  | |__     /  \  | \  / | | |    | | | | |  \| |
+    | |  |  __|   / /\ \ | |\/| | | |    | | | | | . ` |
+    | |  | |____ / ____ \| |  | | | |____| | |_| | |\  |
+    |_|  |______/_/    \_\_|  |_| |______|_|\___/|_| \_|                          
+_________________________________________________________
+                                                                      """)
+print(logo)
+print                                                             ("""
+\033[1;97m\033[1;36m\033[1;97m              WELCOME TO OUR TEAM L10N           \033[;0m\033[1;91m\033[1;92m
+\033[1;32m[-] TOOLS TYPE:\033[1;32m FREE
+\033[1;32m[-] AUTHOR    :\033[1;32m TEAM L10N
+\033[1;32m[-] GITHUB    :\033[1;32m Lion165
+\033[1;32m[-] FACEBOOK  :\033[1;32m Lion Mate                                                    """)
+print("[\033[1;37m01\33[1;92m][\033[1;93mA\33[1;92m] START SETUP BY TEAM L10N")
+print("[\033[1;37m02\33[1;92m][\033[1;93mB\33[1;92m] EXIT")
+print("[\033[1;37m03\33[1;92m][\033[1;93mC\33[1;92m] EXIT")
+print("[\033[1;37m04\33[1;92m][\033[1;93mD\33[1;92m] EXIT")
+opt=input("Choose >>> ")
+if opt in ["A","01"]:
+    pass
+if opt in ["B","02"]:
+    exit()
+if opt in ["C","03"]:
+    exit()                          
+if opt in ["D","04"]:
+	exit()
+os.system('clear')
+os.system('termux-setup-storage')
+os.system('apt update')
+os.system('apt upgrade')
+os.system('pkg update')
+os.system('pkg upgrade')
+os.system('pkg install neofetch')
+os.system('apt install python')
+os.system('apt install python2')
+os.system('apt install python3')
+os.system('apt install git')
+os.system('pkg install nano')
+os.system('pip install bs4')
+os.system('pip install mechanize')
+os.system('pip install requests')
+os.system('pkg install wget')
+os.system('apt install php')
+os.system('apt install curl')  
+os.system('pip install bash')
+os.system('apt install ruby')
+os.system('apt install figlet')
+os.system('pkg install toilet')  
+os.system('pkg install pip')    
+os.system('pkg install pip2')
+os.system('pip2 install requests')
+os.system('pip install future')
+os.system('pip2 install requirements')
+os.system('apt install openssh')
+os.system                                           ("""
+pkg update -y && pkg upgrade -y && pkg install git -y && pkg install python -y && pkg install python2 -y && pip install requests && pip install mechanize && pip2 install requests && pip2 install mechanize && pkg install php && pkg install ruby""")  
+os.system('pkg install fish')       
+os.system('pkg install help')          
+os.system('pkg install perl')
+os.system('pkg install dnsutils')
+os.system('pkg install lua')
+os.system('pog install parallel')
+os.system('pkg install nmap')
+os.system('pkg install clang')
+os.system('pkg install w3m')
+os.system('pkg install hydra')
+os.system('pkg install cowsay')
+os.system('pkg install tar')
+os.system('pkg install zip')
+os.system('pkg install unzip')
+os.system('pkg install net-tools')
+os.system('pkg install tor -y')
+os.system('pkg install sudo -y')
+os.system('pkg install wireshark')
+os.system('pkg install wgetrc')
+os.system('pkg install wcalc')
+os.system('pkg install openssl')
+os.system('pkg install openssl-tool')
+os.system('pkg install bmon')
+os.system('pkg install vpn')
+os.system('pkg install unrar')
+os.system('pkg install proot')
+os.system('pkg install vim')
+os.system('pkg install vim-python')
+os.system('pkg install goaccess')
+os.system('pkg install cmatrix')
+os.system('pkg install kibi')
+os.system('pkg install tsu')
+os.system('pkg install tmux')
+os.system('pkg install pass')
+os.system('pkg install screen')
+os.system('pkg install cvs')
+os.system('pkg install ffmpeg')
+os.system('pkg install gatling')
+os.system('pkg install wren')
+os.system('pkg install netcat')
+os.system('pkg install fakeroot')
+os.system('pkg install xmlsstarlet')
+os.system('pkg install ired')
+os.system('pkg install golang')
+os.system('pkg install swift')
+os.system('pkg install elixir')
+os.system('pkg install ninja')
+os.system('pkg install macchanger')
+os.system('pkg install chroot')
+os.system('termux-chroot')
+os.system('pkg install file')
+os.system('pkg install vis')
+os.system('pkg install pick')
+os.system('pkg install mtools')
+print("\033[1;36m SETUP DONE BY KING TEAM L10N")
